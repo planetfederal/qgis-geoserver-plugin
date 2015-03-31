d65bfeeb1a5cf9cf7e198e44394eabb34a0fe0a6
volaya <volayaf@gmail.com>
2015-03-31 15:00:59 +0200

    changed RetryCatalog implementation to avoid pickling issues

--- src/geoserverexplorer/geoserver/retry.py
+++ src/geoserverexplorer/geoserver/retry.py
@@ -1,4 +1,7 @@
+from functools import wraps
 from geoserver.catalog import Catalog
+import httplib2 
+from urlparse import urlparse
 
 def retryMethodDecorator(func):
     def decorator(*args, **kwargs):
@@ -13,9 +16,29 @@ def retryMethodDecorator(func):
     return decorator
 
 class RetryCatalog(Catalog):
+
+    def setup_connection(self):
+        self.http = RetryConnection(
+            disable_ssl_certificate_validation=self.diable_ssl_cert_validation)
+        self.http.add_credentials(self.username, self.password)
+        netloc = urlparse(self.service_url).netloc
+        self.http.authorizations.append(
+            httplib2.BasicAuthentication(
+                (self.username, self.password),
+                netloc,
+                self.service_url,
+                {},
+                None,
+                None,
+                self.http
+            ))
+
+
+class RetryConnection(httplib2.Http):
     def __getattribute__(self, attr_name):
-        obj = super(Catalog, self).__getattribute__(attr_name)
-        if hasattr(obj, '__call__'):
-            return retryMethodDecorator(obj)
+        obj = super(httplib2.Http, self).__getattribute__(attr_name)
+        if hasattr(obj, '__call__') and hasattr(obj, '__name__'):
+            if not obj.__name__.startswith('__'):
+                return retryMethodDecorator(obj)
         return obj
 
