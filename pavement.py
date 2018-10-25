8ca421b563f56826e56baa7807c8db27f17ffe4b
volaya <volayaf@gmail.com>
2018-10-25 13:53:18 +0200

    Use pip3 in paver setup

--- pavement.py
+++ pavement.py
@@ -88,7 +88,7 @@ def setup(options):
     runtime, test = read_requirements()
     os.environ['PYTHONPATH']=ext_libs.abspath()
     for req in runtime + test:
-        sh('easy_install -a -d %(ext_libs)s %(dep)s' % {
+        sh('pip3 install -U -t %(ext_libs)s %(dep)s' % {
             'ext_libs' : ext_libs.abspath(),
             'dep' : req
         })
